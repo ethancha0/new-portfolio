ddabf1790184047a97177896ca3ee37eeb9742ea
ethancha0 <ethanchao2005@gmail.com>
2026-02-17 08:55:20 -0800

    fix: typescript import error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import MagicBento from "@/components/MagicBento"
 import Project from "@/components/project_row/project"
 import Header from "@/components/header"
 import ZotMeetPicture from "@/public/ZotMeetSS.png"
