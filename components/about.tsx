f91448f8e8b1c934cd20783f97320c7be8b06457
ethancha0 <ethanchao2005@gmail.com>
2026-02-25 19:27:03 -0800

    feat: headshot, updated job finder product img

--- components/about.tsx
+++ components/about.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import ChevronButton from './ui/chevron-button'
+import Image from 'next/image'
+import HeadShot from '@/public/headshot.jpeg'
 
 const About = () => {
   return (
     <div className="flex gap-110 ">
 
-        <div>
+        <div className="">
             <div className="max-w-4xl">
             <h2 className="text-3xl font-semibold">About Me</h2>
           </div>
 
-        <p className="mt-4 text-gray-700">
-            I'm currently studying Software Engineering at UC Irvine <br/>with a passion for building 
-            full-stack applications <br/> <br/>
+        <div className="flex gap-50 justify-center items-center">
+          
+          <p className="mt-4 text-gray-700">
+              I'm currently studying Software Engineering at UC Irvine <br/>with a passion for building 
+              full-stack applications <br/> <br/>
 
-            My work ranges from collaborative tools like ZotMeet to solving<br/> problems with JobFinder.
-            I'm also a proud committee member of <br/>UC Irvine's Infomation & Computer Science Student Council <br/><br/>
+              My work ranges from collaborative tools like ZotMeet to solving<br/> problems with JobFinder.
+              I'm also a proud committee member of <br/>UC Irvine's Infomation & Computer Science Student Council <br/><br/>
 
-            When I'm not programming, you can find me collaboarting with <br/>student organizations like 
-            Tomo no Kai and FUSION on campus.<br/> I also enjoy playing electric guitar, basketball, bouldering, and growing bonsai 
-        </p>
+              When I'm not programming, I'm playing electric guitar, basketball,<br/> bouldering, or growing bonsai 
+
+            
+          </p>
+          <Image src={HeadShot} alt="ethan" className="w-50 h-75 rounded-2xl shadow-2xl"></Image>
+        </div>
 
          <ChevronButton
           redirect={"contactScroll"}
